f86ff173a87e263acbc91db4be7e1368163ee9d6
Hyunjin Cho <hyunjin09222@gmail.com>
2024-02-15 17:26:28 +0900

    카카오 애드핏 추가

--- _head.php
+++ _head.php
@@ -1,4 +1,10 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-include_once(G5_PATH.'/head.php');
+include_once(G5_PATH.'/head.php');?>
+
+<ins class="kakao_ad_area" style="display:none;"
+data-ad-unit = "DAN-mDrxvKx1MWRqXxit"
+data-ad-width = "320"
+data-ad-height = "50"></ins>
+<script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
--- theme/lineup/mobile/head.php
+++ theme/lineup/mobile/head.php
@@ -21,7 +21,11 @@ include_once(G5_LIB_PATH . '/popular.lib.php');
 
     
 </header>
-
+<ins class="kakao_ad_area" style="display:none;"
+data-ad-unit = "DAN-mDrxvKx1MWRqXxit"
+data-ad-width = "320"
+data-ad-height = "50"></ins>
+<script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
 <div id="wrapper">
 
     <div id="container">

fe7fa3af53cc9189754b240b31c927d285a0a6ee
Hyunjin Cho <hyunjin09222@gmail.com>
2024-02-27 16:26:22 +0900

    adsense 사이즈 변경

--- theme/lineup/mobile/index.php
+++ theme/lineup/mobile/index.php
@@ -15,7 +15,6 @@ include_once(G5_THEME_MOBILE_PATH . '/head.php');
 <!-- 메인화면 최신글 시작 -->
 <?php
     $req = isset($_GET['req']) ? $_GET['req'] : null;
-
     switch ($req) {
         case null:
             if ($is_member)
@@ -25,6 +24,7 @@ include_once(G5_THEME_MOBILE_PATH . '/head.php');
             // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
             // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정    
             echo latest('theme/basic', 'notice', 4, 23);		// 최소설치시 자동생성되는 공지사항게시판
+            echo googleAd('banner');
             echo latest('theme/basic', 'free', 4, 23);		// 최소설치시 자동생성되는 자유게시판
             echo latest('theme/basic', 'qa', 4, 23);			// 최소설치시 자동생성되는 질문답변게시판            
             break;
